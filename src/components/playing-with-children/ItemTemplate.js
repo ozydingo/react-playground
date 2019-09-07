8f7ee96f5589b10c533c3c79ff350e4f823ada75
Andrew Schwartz <ozydingo@gmail.com>
2019-09-07 10:15:07 -0400

    feat(PlayingWithChildren) Add fa icon to item template

--- src/components/playing-with-children/ItemTemplate.js
+++ src/components/playing-with-children/ItemTemplate.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function ItemTemplate(props) {
   return (
     <div className={css(styles.warning)}>
-      <b>Warning!</b><br />
+      <div className={css(styles.title)}>
+        <b>
+          <FontAwesomeIcon icon={['fas', 'exclamation-triangle']}/>
+          Warning!
+        </b>
+      </div>
       {props.children}
     </div>
   );
@@ -16,6 +23,9 @@ ItemTemplate.propTypes = {
 };
 
 const styles = StyleSheet.create({
+  title: {
+    marginBottom: "2px",
+  },
   warning: {
     border: "1px solid #cc8",
     borderTopWidth: "8px",
