14d651840c09b33ccc34e7b4c963310bec82aea2
Andrew Schwartz <ozydingo@gmail.com>
2020-12-06 09:56:10 -0500

    More complex transition component with unmount option, lift styling to consumer, use ontransitionend event

---
+++ src/components/transition/Transition.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import PropTypes from "prop-types";
+
+function Transition(props) {
+  const root = useRef(null);
+  const [styleState, setStyleState] = useState("before");
+  const [mounted, setMounted] = useState(!(props.unmount && !props.in));
+
+  function waxOn() {
+    // mount before transition
+    setStyleState("before");
+    setMounted(true);
+  }
+
+  function waxOff() {
+    // transition before unmount
+    setStyleState("out")
+  }
+
+  function transitionAfterMount() {
+    // // Doesn't work: React may choose not to render until after this state is updated.
+    // // In that case, no css transition will occur because the class is "in" from the start.
+    // setStyleState("in");
+
+    // Workaround:
+    setTimeout(() => setStyleState("in"), 35);
+  }
+
+  function unmountAfterTransition() {
+    const elemenet = root.current;
+    if (!elemenet) { return; }
+
+    elemenet.ontransitionend = () => {
+      elemenet.ontransitionend = null;
+      setMounted(false);
+    }
+  }
+
+  useEffect(() => {
+    if (props.unmount) {
+      props.in ? waxOn() : waxOff();
+    } else {
+      setStyleState(props.in ? "in" : "out");
+    }
+  }, [props.in, props.unmount]);
+
+  // If using unmount, set a delay after mounting to ensure the entrance transition.
+  useEffect(() => {
+    if (props.unmount && mounted) { transitionAfterMount(); }
+  }, [mounted, props.unmount]);
+
+  useEffect(() => {
+    if (props.unmount && !props.in) { unmountAfterTransition(); }
+  }, [props.in, props.unmount]);
+
+  if (props.unmount && !mounted) { return null; }
+
+  const className = (props.transition.root || "") + " " + props.transition[styleState] ;
+
+  return (
+    <div ref={root} className={className}>
+      {props.children}
+    </div>
+  );
+}
+
+Transition.propTypes = {
+  children: PropTypes.node,
+  duration: PropTypes.number,
+  in: PropTypes.bool,
+  transition: PropTypes.shape({
+    root: PropTypes.string,
+    before: PropTypes.string,
+    in: PropTypes.string,
+    out: PropTypes.string,
+  }),
+  unmount: PropTypes.bool,
+};
+
+Transition.defaultProps = {
+  duration: 300,
+  transition: {
+    root: "",
+    initial: "",
+    in: "",
+    out: "",
+  }
+};
+
+export default Transition;
--- src/components/transition/TransitionUsage.js
+++ src/components/transition/TransitionUsage.js
@@ -1,19 +1,53 @@
 import React, { useState } from "react";
 
-import MountTransition from "./MountTransition";
+import { css, StyleSheet } from "aphrodite";
+
+import Transition from "./Transition";
 
 function TransitionUsage() {
   const [show, setShow] = useState(false);
 
+  const transitionStyles = {
+    root: css(styles.root),
+    before: css(styles.before),
+    in: css(styles.in),
+    out: css(styles.out),
+  }
+
   return (
     <>
       <div onClick={() => setShow(!show)}>Toggle</div>
-      <MountTransition in={show} duration={1000}>
+      <Transition
+        in={show}
+        duration={1000}
+        transition={transitionStyles}
+        unmount
+      >
         Hello, world.
-      </MountTransition>
+      </Transition>
       <div>end.</div>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    position: "relative",
+    transitionProperty: "opacity top",
+    transitionDuration: "300ms",
+  },
+  before: {
+    opacity: "0",
+    top: "30px",
+  },
+  in: {
+    opacity: "1",
+    top: "0px",
+  },
+  out: {
+    opacity: "0",
+    top: "-30px",
+  },
+});
 
 export default TransitionUsage;
